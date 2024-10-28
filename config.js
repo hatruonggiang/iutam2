a59fef554bc26fd1c211adb2f8367c9bafb77d48
Hà Trường Giang <125450482+hatruonggiang@users.noreply.github.com>
2024-10-28 21:24:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "iuTam",
+    introTitle: 'Embe Tâm',
+    introDesc: `Anh yêu Tâm nhiều`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Sao em lại iu anh giữa 8 tỉ người vậy hihi <3',
+    btnReply: 'Gửi feedback cho anh ngiu <3',
+    reply: 'Do em quá là mê anh rùi <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Vậy thì chần chứ gì mà không nhắn yêu anh nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/e2ee/t/8014905441872595' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
